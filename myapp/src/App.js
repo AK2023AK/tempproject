373a1c9e125d2f356fa79735b53ca97fbcb96224
Anandha Krishnan <u.anandhakrishnan@gmail.com>
2023-11-17 21:59:36 +0530

    Updated My Code

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>Welcome to React js</h1>
+     <h1>Welcome to React js and react native</h1>
     </div>
   );
 }
